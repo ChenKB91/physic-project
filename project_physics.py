db8e94f16cfb9bd6f5b7a67abe201ddb9b0439da
ChenKB91 <38693745+ChenKB91@users.noreply.github.com>
2018-06-08 13:41:57 +0800

    ADD MOAR BALLS
    :)

--- project_physics.py
+++ project_physics.py
@@ -12,13 +12,19 @@ ver 1.2 - fix "ball stuck in wall" problem
 
 '''
 from visual import *
-########important variables##########
-r = 0.05
 
+
+########important variables##########
+r = 0.05 #radius of ball
+N = 50
 #####functions#####
 def dist(x1,y1,x2,y2):
     return sqrt((x1-x2)**2+(y1-y2)**2)
 def reflect(w,v):   #ball hit wall
+    '''
+    w,v must be list/array/vector with 
+    '''
+    w,v = vector(w),vector(v)
     f = vector(-w[1],w[0],0) #法向量
     unit_f=f/abs(f) #法向量的單位向量
     re = v + abs(dot(v,f)/abs(f))*unit_f*2
@@ -28,7 +34,7 @@ def reflect(w,v):   #ball hit wall
         if dot(v,f)<0:
             return re
         else:
-            print('!!!!!!!!!!!!!!!!false hit!!!!!!!!!!!!!!!')
+            #print('!!!!!!!!!!!!!!!!false hit!!!!!!!!!!!!!!!')
             return v
     else:
         #print("back hit")
@@ -39,7 +45,7 @@ def reflect(w,v):   #ball hit wall
             re = v + abs(dot(v,f)/abs(f))*unit_f*2
             return re
         else:
-            print('!!!!!!!!!!!!!!!!false hit!!!!!!!!!!!!!!!')
+            #print('!!!!!!!!!!!!!!!!false hit!!!!!!!!!!!!!!!')
             return v
 #'''
 def vcollision(a1p, a2p, a1v,a2v): #ball hit ball
@@ -65,8 +71,8 @@ def checkhit(w1,w2,b):
     else: return False    
 #initialize!
 scene = display(width=800, height=800,background=(0.0,0.0,0))
-wall = [[1,1],[1,-1],[-1,-1],[-0.8,0],[0,0],[0,-0.8],[1,1]]  #L shape
-#wall = [[1,1],[1,-1],[-1,-1],[-1,1],[1,1]]   #square
+#wall = [[1,1],[1,-1],[-1,-1],[-0.8,0],[0,0],[0,-0.8],[1,1]]  #L shape
+wall = [[2,2],[2,-2],[-2,-2],[-2,2],[2,2]]   #square
 #wall = [[1,1],[1,0],[-1,-1],[-1,0],[1,2],[1,3]]
 #wall = [[780,0],[1150,-140],[1180,-130],[1170,-90],[970,0],[780,0]]
 #wall = [[0,60],[300,60],[850,240],[900,250],[940,220],[950,170],[940,130],[900,100],[730,60],[1170,40],
@@ -74,26 +80,50 @@ wall = [[1,1],[1,-1],[-1,-1],[-0.8,0],[0,0],[0,-0.8],[1,1]]  #L shape
 #        [1400,0],[1070,0],[1200,-60],[1230,-90],[1240,-130],[1230,-170],[1180,-190],[1130,-180],[610,0],[300,20],
 #        [0,0],[0,60]]
 container = curve(pos=wall) 
-
-v    = vector(-10,10)
+random.seed(1)
+v = random.uniform(-10,10,(N,3))
+for i in range(len(v)):
+    v[i][2] = 0
 for i in range(len(wall)):
     wall[i] = vector(wall[i])
 
-ball = sphere(pos = vector(0.5,-0.5,0),radius = r,make_trail=True,retain=100,color=color.yellow)
+pos_arr = random.uniform(-2,2,(N,3))
+for i in range(len(pos_arr)):
+    pos_arr[i][2] = 0
+ball = [sphere(radius = r,make_trail=False,
+               color=color.yellow) for i in range(N)]
+
+ball[0].color = color.red
+ball[0].make_trail = True
+for i in range(N):
+    ball[i].pos = vector(pos_arr[i])
 #testing area    
 
 #main code
 t = 0
 dt =0.0005
+
 while True:
     rate(500)
     t+=dt
-    ball.pos += v*dt
-    for i in range(len(wall)-1):
-        w = wall[i]-wall[i+1]
-        f = vector(-w[1],w[0],0) #法向量
-        if checkhit(wall[i],wall[i+1],ball.pos) and dot(v,f)<0:
-            print("hit: wall %d and %d"%(i,i+1))
-            v = reflect(wall[i]-wall[i+1],v)
-            print(t,abs(v))
+    for j in range(N):
+        for k in range(2):
+            pos_arr[j][k] += v[j][k]*dt
+        ball[j].pos = vector(pos_arr[j])
+        #'''
+        r_array = pos_arr-pos_arr[:,newaxis]            # all pairs of atom-to-atom vectors
+        rmag = sum(square(r_array),-1)                      # atom-to-atom distance squared
+        hit = nonzero((rmag < 4*r**2) - identity(N))
+        hitlist = zip(hit[0], hit[1])     
+        for p,q in hitlist:
+            if sum((pos_arr[p]-pos_arr[q])*(v[p]-v[q])) < 0 :       # check if approaching
+                v[p], v[q] = vcollision(pos_arr[p], pos_arr[q], v[p], v[q])
+        #'''
+        for i in range(len(wall)-1):
+            w = wall[i]-wall[i+1]
+            f = vector(-w[1],w[0]) #法向量
+            if checkhit(wall[i],wall[i+1],ball[j].pos) and dot([v[j][0],v[j][1],0],f)<0:
+                #print("hit: wall %d and %d"%(i,i+1))
+                v[j] = reflect(wall[i]-wall[i+1],v[j])
+                #print(t,abs(v))
             
